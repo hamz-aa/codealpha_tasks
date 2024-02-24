22eabc42cf3a19c52628459cc849f3776eafd1d6
Hamza <jhamzaahmed@gmail.com>
2024-02-24 22:15:01 +0500

    started task 03

---
+++ task-03-social-media-dashboard/js/script.js
@@ -0,0 +1,17 @@
+// sidebar toggle
+let sidebarOpen = false;
+let sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
